3349c4bfa41204ede40a96cd539532b161876679
NaufalAbidAurizky <naufalabidau@gmail.com>
2025-02-26 20:23:27 +0700

    LP3

---
+++ Week3/Code/Matakuliah.java
@@ -0,0 +1,15 @@
+package Week3.Code;
+
+public class Matakuliah {
+    public String Kode;
+    public String Nama;
+    public int SKS;
+    public int JumlahJam;
+
+    public Matakuliah(String Kode, String Nama, int SKS, int JumlahJam) {
+        this.Kode = Kode;
+        this.Nama = Nama;
+        this.SKS = SKS;
+        this.JumlahJam = JumlahJam;
+    }
+}
---
+++ Week3/Code/MatakuliahDemo.java
@@ -0,0 +1,38 @@
+package Week3.Code;
+import java.util.Scanner;
+public class MatakuliahDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Matakuliah[] arrayOfMatakuliah = new Matakuliah[3];
+        String kode, nama, dummy;
+        int sks, jumlahJam;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Masukkan Data Mata Kuliah ke-" + (i + 1));
+            System.out.print("Kode    : ");
+            kode = sc.nextLine();
+            System.out.print("Nama    : ");
+            nama = sc.nextLine();
+            System.out.print("SKS     : ");
+            dummy = sc.nextLine();
+            sks = Integer.parseInt(dummy);
+            System.out.print("Jumlah Jam : ");
+            dummy = sc.nextLine();
+            jumlahJam = Integer.parseInt(dummy);
+            System.out.println("------------------------");
+
+            arrayOfMatakuliah[i] = new Matakuliah(kode, nama, sks, jumlahJam);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Data Mata Kuliah ke-" + (i + 1));
+            System.out.println("Kode    : " + arrayOfMatakuliah[i].Kode);
+            System.out.println("Nama    : " + arrayOfMatakuliah[i].Nama);
+            System.out.println("SKS     : " + arrayOfMatakuliah[i].SKS);
+            System.out.println("Jumlah Jam : " + arrayOfMatakuliah[i].JumlahJam);
+            System.out.println("------------------------");
+        }
+        sc.close();
+    }
+}
