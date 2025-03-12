f9b36f99cbefc7a25be0f8fdbec6284277e57190
NaufalAbidAurizky <naufalabidau@gmail.com>
2025-03-12 22:34:26 +0700

    Pangkat

---
+++ Week5/Code/MainPangkat.java
@@ -0,0 +1,32 @@
+package Week5.Code;
+import java.util.Scanner;
+public class MainPangkat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    
+        System.out.print("Masukkan Jumlah Elemen : ");
+        int elemen = sc.nextInt();
+
+        Pangkat[] png = new Pangkat[elemen];
+        for (int i = 0; i < elemen; i++) {
+            System.out.print("Masukkan Nilai Baris Elemen ke-" + (i+1) + " : ");
+            int basis = sc.nextInt();
+
+            System.out.print("Masukkan Nilai Pangkat Elemen ke-" + (i+1) + " : ");
+            int pangkat = sc.nextInt();
+
+            png[i] = new Pangkat(basis, pangkat);
+        }
+
+        System.out.println("HASIL PANGKAT BRUTEFORCE");
+        for (Pangkat p : png) {
+            System.out.println(p.nilai+"^"+p.pangkat+": "+p.pangkatBF(p.nilai, p.pangkat));
+        }
+        
+        System.out.println("HASIL PANGKAT DIVIDE AND CONQUER");
+        for (Pangkat p : png) {
+            System.out.println(p.nilai+"^"+p.pangkat+": "+p.pangkatDC(p.nilai, p.pangkat));
+        }
+    sc.close();
+    }    
+}
