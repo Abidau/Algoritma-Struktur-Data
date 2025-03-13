43d1e64ea54938ba29462431be96e6ea1e18d941
NaufalAbidAurizky <naufalabidau@gmail.com>
2025-03-13 11:11:00 +0700

    Tugas

--- Week5/Code/MainNilai.java
+++ Week5/Code/MainNilai.java
@@ -3,25 +3,77 @@ import java.util.Scanner;
 
 public class MainNilai {
     public static void main(String[] args) {
-        Scanner masukan = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Masukkan jumlah mahasiswa: ");
-        int jumlah = masukan.nextInt();
+        int jumlah = sc.nextInt();
+
+        if (jumlah <= 0) {
+            System.out.println("Jumlah mahasiswa harus lebih dari 0.");
+            sc.close();
+            return;
+        }
+
+        String nama[] = new String[jumlah];
+        String nim[] = new String[jumlah];
+        int tahunMasuk[] = new int[jumlah];
         int nilaiUTS[] = new int[jumlah];
+        int nilaiUAS[] = new int[jumlah];
+
+        sc.nextLine();
 
         for (int i = 0; i < jumlah; i++) {
-            System.out.print("Masukkan nilai UTS mahasiswa ke-" + (i + 1) + ": ");
-            nilaiUTS[i] = masukan.nextInt();
+            System.out.println("Mahasiswa ke-" + (i + 1));
+            System.out.print("Masukkan Nama Mahasiswa: ");
+            nama[i] = sc.nextLine();
+
+            System.out.print("Masukkan NIM Mahasiswa: ");
+            nim[i] = sc.next();
+
+            System.out.print("Masukkan Tahun Masuk Mahasiswa: ");
+            tahunMasuk[i] = sc.nextInt();
+
+            System.out.print("Masukkan nilai UTS mahasiswa: ");
+            nilaiUTS[i] = sc.nextInt();
+
+            System.out.print("Masukkan nilai UAS mahasiswa: ");
+            nilaiUAS[i] = sc.nextInt();
+            sc.nextLine();
         }
 
         int maksimum = NilaiUTSTertinggi.cariMaksimumDC(nilaiUTS, 0, jumlah - 1);
         int minimum = NilaiUTSTerendah.cariMinimumDC(nilaiUTS, 0, jumlah - 1);
         double rataRata = RataRataNilaiUAS.hitungRataRataBF(nilaiUTS);
 
-        System.out.println("Nilai UTS tertinggi: " + maksimum);
-        System.out.println("Nilai UTS terendah: " + minimum);
-        System.out.println("Rata-rata nilai UAS: " + rataRata);
+        // System.out.println("Nilai UTS tertinggi: " + maksimum);
+        // System.out.println("Nilai UTS terendah: " + minimum);
+        // System.out.println("Rata-rata nilai UAS: " + rataRata);
 
-        masukan.close();
+        int indeksMaks = -1, indeksMin = -1;
+        for (int i = 0; i < jumlah; i++) {
+            if (nilaiUTS[i] == maksimum) {
+                indeksMaks = i;
+            }
+            if (nilaiUTS[i] == minimum) {
+                indeksMin = i;
+            }
+        }
+
+        System.out.println("Mahasiswa dengan nilai UTS tertinggi");
+        System.out.println("\nNilai UTS tertinggi: " + maksimum);
+        System.out.println("Nama: " + nama[indeksMaks]);
+        System.out.println("NIM: " + nim[indeksMaks]);
+        System.out.println("Tahun Masuk: " + tahunMasuk[indeksMaks]);
+
+        System.out.println("Mahasiswa dengan nilai UTS terendah");
+        System.out.println("\nNilai UTS terendah: " + minimum);
+        System.out.println("Nama: " + nama[indeksMin]);
+        System.out.println("NIM: " + nim[indeksMin]);
+        System.out.println("Tahun Masuk: " + tahunMasuk[indeksMin]);
+
+        System.out.println("\nRata-rata nilai UAS: " + rataRata);
+
+        sc.close();
     }
 }
+
