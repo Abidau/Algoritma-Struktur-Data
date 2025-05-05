8e836e10a516622c210d662b4b192196418a9137
NaufalAbidAurizky <naufalabidau@gmail.com>
2025-05-05 20:38:33 +0700

    Latihan Praktikum

---
+++ Week9/Code/Surat.java
@@ -0,0 +1,24 @@
+package Week9.Code;
+
+public class Surat {
+    String idSurat, namaMahasiswa, kelas;
+    char jenisIzin;
+    int durasi;
+
+    public Surat(String idSurat, String namaMahasiswa, String kelas, char jenisIzin, int durasi) {
+        this.idSurat = idSurat;
+        this.namaMahasiswa = namaMahasiswa;
+        this.kelas = kelas;
+        this.jenisIzin = jenisIzin;
+        this.durasi = durasi;
+    }
+
+    public void tampilkan() {
+        System.out.println("ID Surat       : " + idSurat);
+        System.out.println("Nama Mahasiswa : " + namaMahasiswa);
+        System.out.println("Kelas          : " + kelas);
+        System.out.println("Jenis Izin     : " + jenisIzin);
+        System.out.println("Durasi         : " + durasi + " hari");
+        System.out.println("-----------------------------");
+    }
+}
