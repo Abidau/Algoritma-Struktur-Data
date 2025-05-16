bd3390161a5ca9bbd582d319fbbd4ce11b446fcb
NaufalAbidAurizky <naufalabidau@gmail.com>
2025-05-16 21:11:10 +0700

    Tugas

---
+++ Week10/Code/Mahasiswa20.java
@@ -0,0 +1,19 @@
+package Week10.Code;
+
+public class Mahasiswa20 {
+    String nim;
+    String nama;
+    String prodi;
+    String kelas;
+
+    Mahasiswa20(String nim, String nama, String prodi, String kelas) {
+        this.nim = nim;
+        this.nama = nama;
+        this.prodi = prodi;
+        this.kelas = kelas;
+    }
+
+    void tampilkanData() {
+        System.out.println("NIM: " + nim + ", Nama: " + nama + ", Prodi: " + prodi + ", Kelas: " + kelas);
+    }
+}
