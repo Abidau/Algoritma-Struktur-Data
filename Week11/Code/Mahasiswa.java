13ff295c2224d72b40516d663eb186511e8768d3
NaufalAbidAurizky <naufalabidau@gmail.com>
2025-05-22 22:07:01 +0700

    Tugas Praktikum

---
+++ Week11/Code/Mahasiswa.java
@@ -0,0 +1,12 @@
+public class Mahasiswa {
+    String nim, nama;
+
+    public Mahasiswa(String nim, String nama) {
+        this.nim = nim;
+        this.nama = nama;
+    }
+
+    public void tampil() {
+        System.out.println("NIM: " + nim + ", Nama: " + nama);
+    }
+}
---
+++ Week11/Code/MainQueue.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class MainQueue {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        QueueLinkedList antrian = new QueueLinkedList();
+
+        int pilih;
+        do {
+            System.out.println("\n=== Menu Layanan Kemahasiswaan ===");
+            System.out.println("1. Daftar & Tambah Antrian");
+            System.out.println("2. Panggil Antrian");
+            System.out.println("3. Lihat Antrian Terdepan");
+            System.out.println("4. Lihat Antrian Terakhir");
+            System.out.println("5. Lihat Semua Antrian");
+            System.out.println("6. Lihat Jumlah Antrian");
+            System.out.println("7. Kosongkan Antrian");
+            System.out.println("0. Keluar");
+            System.out.print("Pilih: ");
+            pilih = sc.nextInt();
+            sc.nextLine(); // membersihkan newline
+
+            switch (pilih) {
+                case 1:
+                    System.out.print("Masukkan NIM: ");
+                    String nim = sc.nextLine();
+                    System.out.print("Masukkan Nama: ");
+                    String nama = sc.nextLine();
+                    Mahasiswa mhs = new Mahasiswa(nim, nama);
+                    antrian.enqueue(mhs);
+                    break;
+                case 2:
+                    antrian.dequeue();
+                    break;
+                case 3:
+                    antrian.peekFront();
+                    break;
+                case 4:
+                    antrian.peekRear();
+                    break;
+                case 5:
+                    antrian.printQueue();
+                    break;
+                case 6:
+                    System.out.println("Jumlah mahasiswa dalam antrian: " + antrian.getSize());
+                    break;
+                case 7:
+                    antrian.clear();
+                    break;
+                case 0:
+                    System.out.println("Terima kasih!");
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid.");
+            }
+        } while (pilih != 0);
+    sc.close();
+    }
+}
