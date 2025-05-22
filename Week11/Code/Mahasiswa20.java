bd8492370e4308039e4f987d75b10423a49dc97e
NaufalAbidAurizky <naufalabidau@gmail.com>
2025-05-22 21:50:35 +0700

    Percobaan Praktikum

---
+++ Week11/Code/Mahasiswa20.java
@@ -0,0 +1,24 @@
+public class Mahasiswa20 {
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    Mahasiswa20() {
+    }
+
+    Mahasiswa20(String nm, String name, String kls, double ip) {
+        nim = nm;
+        nama = name;
+        kelas = kls;
+        ipk = ip;
+    }
+
+    void tampilInformasi() {
+        System.out.println("NIM   : " + nim);
+        System.out.println("Nama  : " + nama);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("IPK   : " + ipk);
+        System.out.println("------------------------");
+    }
+}
---
+++ Week11/Code/SLLMain20.java
@@ -0,0 +1,46 @@
+public class SLLMain20 {
+    public static void main(String[] args) {
+        SingleLinkedList20 sll = new SingleLinkedList20();
+
+        Mahasiswa20 mhs1 = new Mahasiswa20("20001", "Alvaro", "3A", 3.5);
+        Mahasiswa20 mhs2 = new Mahasiswa20("20002", "Contia", "3B", 3.2);
+        Mahasiswa20 mhs3 = new Mahasiswa20("20003", "Bimon", "3A", 3.8);
+        Mahasiswa20 mhs4 = new Mahasiswa20("20004", "Dirga", "3C", 3.1);
+        
+        // sll.print();
+        // sll.addFirst(mhs4);
+        // sll.print();
+        // sll.addLast(mhs1);
+        // sll.print();
+        // sll.insertAfter("Dirga", mhs3);
+        // sll.insertAt(2, mhs2);
+        // sll.print();
+
+        sll.addFirst(mhs1);
+        sll.addLast(mhs2);
+        sll.addLast(mhs3);
+        sll.addLast(mhs4);
+
+        System.out.println("Data awal: ");
+        sll.print();
+
+        System.out.println("Data Index 1: ");
+        sll.getData(1).tampilInformasi();
+
+        System.out.println("Data Mahasiswa an Bimon berada pada index: " + sll.indexOf("Bimon"));
+        System.out.println();
+
+        sll.removeFirst();
+        System.out.println("Setelah dihapus dari depan: ");
+        sll.print();
+
+        sll.removeLast();
+        System.out.println("Setelah dihapus dari belakang: ");
+        sll.print();
+
+        sll.removeAt(0);
+        System.out.println("Setelah dihapus index 0: ");
+        sll.print();
+    }
+}
+
